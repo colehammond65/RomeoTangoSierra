7c577215d46dfaa04430eace20eb61250a6d46ff
Cole Hammond <cole.d.hammond@hotmail.co.nz>
2022-09-17 20:38:39 +1200

    28. Unit Base Building

--- Assets/_Scripts/Networking/RTSNetworkManager.cs
+++ Assets/_Scripts/Networking/RTSNetworkManager.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using UnityEngine.SceneManagement;
 
 public class RTSNetworkManager : NetworkManager
 {
 
     [SerializeField] private GameObject unitSpawnerPrefab = null;
+    [SerializeField] private GameOverHandler gameOverHandlerPrefab = null;
 
     public override void OnServerAddPlayer(NetworkConnectionToClient conn)
     {
@@ -15,5 +17,13 @@ public class RTSNetworkManager : NetworkManager
         GameObject uniSpawnerInstance = Instantiate(unitSpawnerPrefab, conn.identity.transform.position, conn.identity.transform.rotation);
 
         NetworkServer.Spawn(uniSpawnerInstance, conn);
+    }
+
+    public override void OnServerChangeScene(string sceneName)
+    {
+        if(SceneManager.GetActiveScene().name.StartsWith("Scene_Map")){
+            GameOverHandler gameOverHandlerInstance = Instantiate(gameOverHandlerPrefab);
+            NetworkServer.Spawn(gameOverHandlerInstance.gameObject);
+        }
     }
 }
