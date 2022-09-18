9c0468dcccf210c9976b724998662eb34f97fe6d
Cole Hammond <cole.d.hammond@hotmail.co.nz>
2022-09-18 13:12:10 +1200

    29. Winning The Game

---
+++ Assets/_Scripts/UI/GameOverDisplay.cs
@@ -0,0 +1,33 @@
+using TMPro;
+using UnityEngine;
+using Mirror;
+
+public class GameOverDisplay : MonoBehaviour
+{
+    [SerializeField] private GameObject gameOverDisplayParent = null;
+    [SerializeField] private TMP_Text winnerNameText = null;
+
+    void Start()
+    {
+        GameOverHandler.ClientOnGameOver += ClientHandleGameOver;
+    }
+
+    private void OnDestroy(){
+        GameOverHandler.ClientOnGameOver -= ClientHandleGameOver;
+    }
+
+    public void LeaveGame(){
+        if(NetworkServer.active && NetworkClient.isConnected){
+            NetworkManager.singleton.StopHost();
+        }
+        else{
+            NetworkManager.singleton.StopClient();
+        }
+    }
+
+    private void ClientHandleGameOver(string winner){
+        winnerNameText.text = $"{winner} Has Won!";
+
+        gameOverDisplayParent.SetActive(true);
+    }
+}
